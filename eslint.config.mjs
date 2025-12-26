1d5ee401660c7cbf29038c345e6f8abd3088022d
JoostWG <122867812+JoostWG@users.noreply.github.com>
2025-12-26 11:42:04 +0100

    :truck: Move to docs-build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig(
-    globalIgnores(['dist/*', 'docs/*', 'eslint.config.mjs', 'typedoc-plugin.mjs']),
+    globalIgnores(['dist/*', 'docs-build/*', 'eslint.config.mjs', 'typedoc-plugin.mjs']),
     js.configs.all,
     tseslint.configs.all,
     {
