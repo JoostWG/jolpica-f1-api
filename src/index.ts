f3e5506f956a7d4b680439c7c0850d1b8fd572e3
JoostWG <122867812+JoostWG@users.noreply.github.com>
2025-12-31 20:12:40 +0100

    :bug: Fix models export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export * from './Api';
 export * from './enums';
 export * from './errors';
-export type * from './models';
+export * from './models';
 export * from './PendingRequest';
 export * from './SimpleApiCache';
 export type * from './types';
