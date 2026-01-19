80a332bd9cce1b4af4db5d05cbe7dd2d250be159
Joost Groen <122867812+JoostWG@users.noreply.github.com>
2026-01-19 13:18:59 +0100

    Refactor openStreetMapUrl method for clarity

--- src/models/Location.ts
+++ src/models/Location.ts
@@ -22,7 +22,9 @@ export class Location extends Model {
         this.country = data.country;
     }
 
-    public openStreetMapUrl<const TZoom extends number>(zoomLevel: TZoom = 14): `https://www.openstreetmap.org/#map=${TZoom}/${number}/${number}` {
-        return `https://www.openstreetmap.org/#map=${zoomLevel}/${this.latitude}${this.longitude}`;
+    public openStreetMapUrl(
+        zoomLevel: number = 14,
+    ): `https://www.openstreetmap.org/#map=${number}/${number}/${number}` {
+        return `https://www.openstreetmap.org/#map=${zoomLevel}/${this.latitude}/${this.longitude}`;
     }
 }
